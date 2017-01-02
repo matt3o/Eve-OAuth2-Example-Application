25ed7985acb8c62e374c42f76ef8b108630c6eba
Cody <cody@quantifiedcode.com>
2017-01-02 22:30:47 +0000

    Migrated `%` string formating

--- flask_app/controller.py
+++ flask_app/controller.py
@@ -4,4 +4,4 @@ from flask_app import fapp
 
 @fapp.route('/<page>', defaults={'page': 'index'})
 def main(page):
-    return render_template('pages/%s.html' % page)
+    return render_template('pages/{0!s}.html'.format(page))
